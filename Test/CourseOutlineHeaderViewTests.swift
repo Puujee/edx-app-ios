fb96c3a332e5f60747fb8c50b9f135bb7f148367
Ehmad Zubair Chughtai <ezubair@gmail.com>
2015-06-25 14:27:57 +0500

    Updated snapshot test

--- Test/CourseOutlineHeaderViewTests.swift
+++ Test/CourseOutlineHeaderViewTests.swift
@@ -10,11 +10,14 @@ import edX
 import Foundation
 
 class CourseOutlineHeaderViewTests : SnapshotTestCase {
+
+    override func setUp() {
+        super.setUp()
+        recordMode = true
+    }
     
     func testProgressView() {
         let progressView = CourseOutlineHeaderView(frame: CGRectZero, styles: OEXStyles(), titleText: OEXLocalizedString("VIDEO_DOWNLOADS_IN_PROGRESS", nil), shouldShowSpinner: true)
-        
-        CourseOutlineHeaderView(frame : CGRectZero, styles : OEXStyles(), shouldShowSpinner : true)
         
         let size = progressView.systemLayoutSizeFittingSize(self.screenSize)
         progressView.bounds = CGRect(x: 0, y: 0, width: size.width, height: size.height)
@@ -23,17 +26,15 @@ class CourseOutlineHeaderViewTests : SnapshotTestCase {
         assertSnapshotValidWithContent(progressView)
     }
     
-    func testLastAccessedView() {
-        let lastAccessedView = CourseOutlineHeaderView(frame: CGRectZero, styles: OEXStyles(), titleText: OEXLocalizedString("LAST_ACCESSED", nil), subtitleText : "This is a very long subtitle which should not overlap the button")
+    func testHeaderInfoView() {
+        let headerInfoView = CourseOutlineHeaderView(frame: CGRectZero, styles: OEXStyles(), titleText: OEXLocalizedString("LAST_ACCESSED", nil), subtitleText : "This is a very long subtitle which should not overlap the button")
         
-        CourseOutlineHeaderView(frame : CGRectZero, styles : OEXStyles(), shouldShowSpinner : true)
-        
-        let size = lastAccessedView.systemLayoutSizeFittingSize(self.screenSize)
+        let size = headerInfoView.systemLayoutSizeFittingSize(self.screenSize)
         // Using 380 to make sure that the subtitle truncates
-        lastAccessedView.bounds = CGRect(x: 0, y: 0, width: 380, height: size.height)
+        headerInfoView.bounds = CGRect(x: 0, y: 0, width: 380, height: size.height)
         
-        lastAccessedView.layoutIfNeeded()
-        assertSnapshotValidWithContent(lastAccessedView)
+        headerInfoView.layoutIfNeeded()
+        assertSnapshotValidWithContent(headerInfoView)
 
     }
 }
